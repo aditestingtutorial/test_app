1518516a12d02032d7b0dd596ad8ec41f077ee14
aditestingtutorials <ady_viper_cena@yahoo.com>
2018-04-12 19:31:40 +0300

    add calss end to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,3 +3,5 @@ class Article < ActiveRecord::Base
 #getters and setters are automatically created when this class is created
 validates :title, presence: true, length: {minimum: 3, maximum: 50}
 validates :description, presence:true, length: {minimum: 10, maximum: 300}
+
+end
