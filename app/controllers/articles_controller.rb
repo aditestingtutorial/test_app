e1049a7032a86be912df1b7265e1e54ad96c2e80
aditestingtutorials <ady_viper_cena@yahoo.com>
2018-04-13 23:02:44 +0300

    Insert,show,edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
 class ArticlesController < ApplicationController
 
+def index
+
+@article = Article.all
+
+end
+
+
 def new
 
 @article = Article.new
@@ -15,6 +22,23 @@ def create
 	else
 		render 'new'
 	end
+end
+
+def edit
+
+@article = Article.find(params[:id])
+
+end
+
+def update
+
+@article = Article.find(params[:id])
+if @article.update(article_params)
+	flash[:notice] = "Article was successfully updated!"
+	redirect_to article_path(@article)
+else
+	render 'edit'
+end
 end
 
 def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-#getters and setters are automatically created when this class is created
+#!!!getters and setters for defined fields are automatically created when this class is created!!!
 validates :title, presence: true, length: {minimum: 3, maximum: 50}
 validates :description, presence:true, length: {minimum: 10, maximum: 300}
 
