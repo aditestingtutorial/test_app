ffffe798130b701ac594be64101629341d51d480
aditestingtutorials <ady_viper_cena@yahoo.com>
2018-04-14 18:05:52 +0300

    Destroy and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,14 @@ else
 end
 end
 
+def destroy
+
+@article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "The article was successfully deleted!"
+redirect_to articles_path
+end
+
 def show
 	@article = Article.find(params[:id])
 end
